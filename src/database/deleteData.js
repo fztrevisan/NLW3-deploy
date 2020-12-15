ca967602bc5c2c3e82a2a63691046eb9de009ab6
Fernando.Trevisan <trevisan.fernando@gmail.com>
2020-12-15 18:39:42 -0300

    Edit script to delete data

--- src/database/deleteData.js
+++ src/database/deleteData.js
@@ -1,7 +1,6 @@
 const Database = require('./db')
 
 Database.then(async db => {
-  await prompt("Tell me the name of the orphanage to DELETE: ", "name")
   // Deletar dados da tabela:
   await db.run("DELETE FROM orphanages WHERE name = '' ")
 
