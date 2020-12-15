6792eb3b3d7b13e8f93bfc6d7b69a068d79da5fd
Fernando.Trevisan <trevisan.fernando@gmail.com>
2020-12-15 18:30:08 -0300

    Script to delete data

--- src/database/deleteData.js
+++ src/database/deleteData.js
@@ -3,11 +3,11 @@ const Database = require('./db');
 module.exports = {
   async deleteOrphanage(request, response) {
     try {
-      const orphanageName = prompt("Tell me the orphanage name to DELETE");
       const db = await Database;
-      const query = await db.all(`DELETE FROM orphanages WHERE name = '${orphanageName}' `)
+      const query = await db.all("DELETE FROM orphanages WHERE name = '' ")
       const orphanages = await db.all("SELECT * FROM orphanages")
-      return console.log(orphanages)
+      console.log(orphanages)
+      // return console.log(orphanages)
     } catch (error) {
       console.log(error)
       return response.send('Erro no banco de dados!')
